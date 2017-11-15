23b09abaccc15c3b43aca8b9b18c0e1a49b6c12a
DeronLee <akagilnc@gmail.com>
2017-11-15 18:32:18 +0800

    fix style error

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import Product from '@/components/product/Product'
 import ProductDetail from '@/components/product/ProductDetail'
 import NotFound from '@/components/common/NotFound'
 import OrderStep1 from '@/components/order/OrderStep1'
-
 // const Order = () => import(/* webpackChunkName: "group-order" */ '@/components/order/Order')
 
 Vue.use(Router)
