80a230060c95ba7e529485008574041abfd031e9
qiuxia.chen <qiuxia.chen@monstar-lab.com>
2017-11-15 15:26:29 +0800

    product list preliminary over

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="product-con">
     <index-commercial/>
+    <p class="title">业务一览</p>
     <product-list/>
   </div>
 </template>
@@ -17,3 +18,10 @@
     }
   }
 </script>
+<style lang="scss" scoped>
+  .title{
+    text-align: center;
+    height: 50px;
+    line-height: 50px
+  }
+</style>
