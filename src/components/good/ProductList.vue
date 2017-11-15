aa364a1244d62e44b8b4d7df8ee2b2a12c0b3c92
qiuxia.chen <qiuxia.chen@monstar-lab.com>
2017-11-15 13:43:26 +0800

    product list

--- src/components/good/ProductList.vue
+++ src/components/good/ProductList.vue
@@ -1,16 +1,34 @@
 <template>
-  <section class="product-list">
-
-
-  </section>
+  <ul>
+    <li class="product-list" v-for="prodcut in products">
+      <img v-bind:src="prodcut.icon" alt="thumbnail">
+      <el-container>
+        <h1>{{prodcut.name}}</h1>
+        <h2>{{prodcut.brief}}</h2>
+        <p>{{prodcut.discription}}</p>
+      </el-container>
+    </li>
+  </ul>
 </template>
-
 <script>
+  import {getProductList} from './Good.service.js'
   export default {
-    name: 'productList'
+    name: 'productList',
+    data:function () {
+      return{
+        products:{}
+      }
+    },
+    created(){
+      getProductList(this)
+      .then(data => {
+        this.products = data
+      })
+      .catch (console.log('product ERROR'))
+    }
   }
 </script>
 
-<style>
+<style lang="scss"scoped>
 
 </style>
