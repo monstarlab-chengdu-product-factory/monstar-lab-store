f09a628a66da8a3d68a01bbb9ef0bb6afc163aa9
woocool <woocool@126.com>
2017-11-15 16:50:36 +0800

    order step1, select main type

---
+++ src/components/order/TypeSelectorList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="type-selector-list">
+    <ul>
+      <li v-for="type in types" :key="type.id">
+        <type-selector :url='type.icon' :name='type.name' :brief='type.brief'/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import { ProductService } from './Product.service.js'
+  import TypeSelector from './TypeSelector'
+  export default {
+    name: 'TypeSelectorList',
+    components: {
+      'type-selector': TypeSelector
+    },
+    data () {
+      return {
+        types: {}
+      }
+    },
+    created () {
+      let service = new ProductService()
+      service.getProducts(this)
+        .then(data => {
+          this.types = data
+        })
+    }
+  }
+</script>
