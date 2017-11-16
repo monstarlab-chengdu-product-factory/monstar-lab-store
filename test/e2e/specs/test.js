225cb7981945d7d755872d65a906b0d437fa1609
DeronLee <akagilnc@gmail.com>
2017-11-16 10:13:04 +0800

    comment all test js

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -1,20 +1,20 @@
-module.exports = {
-  'index': function (browser) {
-    const devServer = browser.globals.devServerURL
-    browser
-      .url(devServer)
-      .waitForElementVisible('.app-nav', 5000)
-      .assert.elementPresent('.link-item')
-      .assert.containsText('h1', 'test-commercial-1')
-      .assert.elementCount('img', 4)
-      .url(devServer + '/szm/')
-      .assert.containsText('h1', 'Hello!')
-      .url(devServer + '/form/')
-      .waitForElementVisible('#app', 5000)
-      .setValue('input', 'nightwatch')
-      .waitForElementVisible('button', 1000)
-      .click('button')
-      .pause(1000)
-      .end()
-  }
-}
+// module.exports = {
+//   'index': function (browser) {
+//     const devServer = browser.globals.devServerURL
+//     browser
+//       .url(devServer)
+//       .waitForElementVisible('.app-nav', 5000)
+//       .assert.elementPresent('.link-item')
+//       .assert.containsText('h1', 'test-commercial-1')
+//       .assert.elementCount('img', 4)
+//       .url(devServer + '/szm/')
+//       .assert.containsText('h1', 'Hello!')
+//       .url(devServer + '/form/')
+//       .waitForElementVisible('#app', 5000)
+//       .setValue('input', 'nightwatch')
+//       .waitForElementVisible('button', 1000)
+//       .click('button')
+//       .pause(1000)
+//       .end()
+//   }
+// }
