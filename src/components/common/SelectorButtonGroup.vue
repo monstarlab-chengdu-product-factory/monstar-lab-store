b3d10cde8fda849fbfc8a080190050ddff3b2fdb
woocool <woocool@126.com>
2017-11-16 16:47:39 +0800

    添加productList相关对象

---
+++ src/components/common/SelectorButtonGroup.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="selector-button-group">
+    <ul>
+      <li v-for="button in buttons" :key="button.id">
+        <selector-button :label='button.label' :selected='button.selected' :removeAble='button.removeAble' :editAble='button.editAble'/>
+      </li>
+      <li v-if="appendAble">
+        <a href="#"><span class="icon icon-add"></span></a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  // import { ProductService } from '../product/Product.service.js'
+  import SelectorButton from './SelectorButton'
+  export default {
+    name: 'SelectorButtonGroup',
+    components: {
+      'selector-button': SelectorButton
+    },
+    props: ['appendAble'],
+    data () {
+      return {
+        buttons: {}
+      }
+    },
+    created () {
+
+    }
+  }
+</script>
---
+++ src/components/product/ProductTypeTab.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="product-type-tab">
+    <div class="cat-selector">
+      <cat-selector-group></cat-selector-group>
+    </div>
+    <div class="type-selector">
+      <type-selector-group></type-selector-group>
+    </div>
+  </div>
+</template>
+
+<script>
+  import SelectorButtonGroup from './SelectorButtonGroup'
+  export default {
+    name: 'ProductTypeTab',
+    components: {
+      'cat-selector-group': SelectorButtonGroup,
+      'type-selector-group': SelectorButtonGroup
+    },
+    props: [],
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
