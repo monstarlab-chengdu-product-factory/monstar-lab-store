f1f33a59852a110ef819733e920f9571e43a7965
woocool <woocool@126.com>
2017-11-17 18:25:33 +0800

    step2 select products

--- src/components/common/SelectorButton.vue
+++ src/components/common/SelectorButton.vue
@@ -9,20 +9,44 @@
   </div>
 </template>
 
+<style lang="scss">
+  @import "../../assets/stylesheet/component_import";
+
+  .selector-button {
+    @include box($display: inline-block, $width: auto, $height: 40px, $line-height: 40px, $text-align: center);
+    @include font-size(16px);
+    @include bd(1px, solid, map-get($global-color, border));
+    @include border-radius (5px);
+    background-color: #fff;
+    cursor: pointer;
+    min-width: rem(100px);
+    padding: rem(10px);
+    position: relative;
+    &.selected {
+      color: #fff;
+      background-color: map-get($global-color-base, primary);
+      border-color: map-get($global-color-base, primary);
+      &:after {
+        content: "";
+        @include triangle($direction: br);
+        @include position-location ($top: auto, $bottom: 0, $left: auto, $right: 0);
+      }
+    }
+  }
+
+</style>
+
 <script>
   export default {
     name: 'SelectorButton',
-    props: ['label', 'selected', 'removeAble', 'editAble'],
+    props: ['proLabel', 'proSelected', 'proRemoveAble', 'proEditAble'],
     data () {
       return {
-        label: this.label,
-        selected: this.selected,
-        removeAble: this.removeAble,
-        editAble: this.editAble
+        label: this.proLabel,
+        selected: this.proSelected,
+        removeAble: this.proRemoveAble,
+        editAble: this.proEditAble
       }
     }
   }
 </script>
-
-<style>
-</style>
