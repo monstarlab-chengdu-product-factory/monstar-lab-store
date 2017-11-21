eee682e2d76b06f87b367beb19a781eeb12099f2
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-11-21 14:39:12 +0800

    11.21 story_orderComfirm

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,7 +1,22 @@
 <template>
   <section>
-    <order-confirm/>
+   <div class="comfirm-con">
+    <header>第三步：报价试算</header>
+    
+    <div class="list-con">
+      <section class="list-item">
+        <header>
+          <p>类型 机能数88</p>
+          <router-link to='/'>调整需求</router-link>
+        </header>
+      </section>
+    </div>
+   </div>
+    <!-- <router-view/> -->
   </section>
 </template>
 <script>
+  export default {
+    name: 'Order'
+  }
 </script>
