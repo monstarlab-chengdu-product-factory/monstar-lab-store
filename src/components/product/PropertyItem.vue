496d91801b6183b14db68fbb000879bbd329faf0
qiuxia.chen <qiuxia.chen@monstar-lab.com>
2017-12-01 13:54:33 +0800

    modify the type tab code

---
+++ src/components/product/PropertyItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li class="function-property">
+      <el-button class="button-father">{{properties}}</el-button>
+      <el-button class="button-child">{{items}}</el-button>
+    </li>
+  </ul>
+</template>
+<script>
+  export default {
+    name: 'FunctionItem',
+    props: {
+      function: {},
+      functionItems: {}
+    },
+    data () {
+      return {
+        properties: this.function,
+        items: this.functionItems
+      }
+    }
+
+  }
+
+</script>
+<style lang='scss' scoped>
+
+</style>
