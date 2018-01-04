bfcdf02942e9d1b5cb754a2153562a03c2a3546e
yiqianliu <qiuxia.chen@monstar-lab.com>
2018-01-04 15:39:52 +0800

    modify

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,8 @@
-import * as types from '../mutation-types'
-export const addToCart = ({commit}, product) => {
-    if(product) {
-      commit(types.ADD_TO_CART, {
-        id: product.id
-      })
-    }
-}
+// import * as types from '../mutation-types'
+// export const addToCart = ({commit}, product) => {
+//   if (product) {
+//     commit(types.ADD_TO_CART, {
+//       id: product.id
+//     })
+//   }
+// }
